26a4c47f1ce2ed1b45177423fe4d965cb483edd5
Louiii <43211625+Louiii@users.noreply.github.com>
2020-03-04 20:56:37 +0000

    v2

--- ABC.py
+++ ABC.py
@@ -4,7 +4,7 @@ def plot(problem, bound, best, name, n=30):
     from mpl_toolkits import mplot3d
     import matplotlib.pyplot as plt
 
-    (xmin, xmax, _) = bound
+    (xmin, xmax) = bound
     x = np.outer(np.linspace(xmin, xmax, n), np.ones(n))
     y = x.copy().T
     z = np.zeros((n, n))
@@ -17,7 +17,7 @@ def plot(problem, bound, best, name, n=30):
     ax = plt.axes(projection='3d')
     ax.view_init(20, 15)
 
-    [xo, yo], zo = list(best.loc), best.cost
+    [xo, yo], zo = list(best.x), best.f
     ax.scatter(xo, yo, zo, color='red', s=10)
 
     ax.plot_surface(x, y, z,cmap='viridis', edgecolor='none', alpha=.6)
@@ -26,87 +26,98 @@ def plot(problem, bound, best, name, n=30):
     plt.show()
 
 class Bee:
-    def __init__(self, loc=[], cost=None):
-        self.loc = loc
-        self.cost = cost
+    def __init__(self, x=[], f=None):
+        self.x = x
+        self.f = f
 
-def ABC(fn, d, bound, param):
+def ABC(fn, d, bound, SN, limit, MCN):
     '''  minimise fn over domain x in [bound]^d  '''
-    (n_iter, n) = param
-    (xmin, xmax, a) = bound
+    (xmin, xmax) = bound
 
-    lim = round(d*n)
-    
-    best = Bee(cost=float('inf'))
-    bee = [Bee() for _ in range(n)]
+    # Scout bees initiate food source, random food positions
+    X = np.random.uniform(low=xmin, high=xmax, size=(SN, d))
+    employed = [Bee(x=X[i], f=fn(X[i])) for i in range(SN)]
+    onlooker = employed[:]
 
-    # Scout bees initiate food source
-    # -> randomly select a set of food source positions for the bees
-    for i in range(n):
-        bee[i].loc = np.random.uniform(low=xmin, high=xmax, size=d)
-        bee[i].cost = fn(bee[i].loc)
-        if bee[i].cost < best.cost:
-            best = bee[i]
-
-    C = np.zeros(n)
-
-    for it in range(n_iter):
+    C = np.zeros(SN)
+    for it in range(MCN):
         # Employed bees; place on the food sources in the memory
         # -> measure nectar amounts
-        for i in range(n):
-            K = list(range(i-1))+list(range(i, n))
+        for i in range(SN):
+            K = list(range(i-1))+list(range(i, SN))
             k = K[np.random.randint(len(K))]
+
+            # j = np.random.randint(d)
+            # phi = np.random.uniform(low=-1, high=1)
+            # v = employed[i].x
+            # v[j] = min(max(v[j] + phi * (v[j] - employed[k].x[j]), xmin), xmax)
             
-            phi = a*np.random.uniform(-1,1,d)[0]
+            phi = np.random.uniform(low=-1, high=1, size=d)
+            v = employed[i].x + np.multiply(phi, (employed[i].x - employed[k].x))
+            v = np.minimum(np.maximum(v, xmin), xmax)
+
+            fv = fn(v)
             
-            newbee = Bee()
-            newbee.loc = bee[i].loc + np.multiply(phi, (bee[i].loc - bee[k].loc))
-            newbee.loc = np.minimum(np.maximum(newbee.loc, xmin), xmax)
-            newbee.cost = fn(newbee.loc)
-            
-            if newbee.cost < bee[i].cost:
-                bee[i] = newbee
+            if fv < employed[i].f:
+                employed[i] = Bee(x=v, f=fv)
             else:
                 C[i] += 1
         
         # Onlooker bees; place on the food sources in the memory
         # -> select the food sources
-        fit = np.zeros(n)
-        for i in range(n):
-            if (bee[i].cost >= 0):
-                fit[i] = 1/(1+bee[i].cost)
+        fit = np.zeros(SN)
+        for i in range(SN):
+            # fit[i] = -employed[i].f
+            if (employed[i].f >= 0):
+                fit[i] = 1/(1+employed[i].f)
             else:
-                fit[i] = 1+abs(bee[i].cost)
+                fit[i] = 1+abs(employed[i].f)
         P = fit/sum(fit)
         
-        for i in range(n):
-            j = np.random.choice(range(len(P)), p=P)
-            K = list(range(j-1))+list(range(j, n))
+        for i in range(SN):
+            # K = list(range(i-1))+list(range(i, SN))
+            # k = K[np.random.randint(len(K))]
+            n = np.random.choice(range(len(P)), p=P/np.sum(P))
+            
+            K = list(range(n-1))+list(range(n, SN))
             k = K[np.random.randint(len(K))]
+
+            # j = np.random.randint(d)
+            # phi = np.random.uniform(low=-1, high=1)
+            # v = employed[n].x
+            # v[j] = min(max(v[j] + phi * (v[j] - employed[k].x[j]), xmin), xmax)
+
+            phi = np.random.uniform(low=-1, high=1, size=d)
+            v = employed[n].x + np.multiply(phi, (employed[n].x - employed[k].x))
+            v = np.minimum(np.maximum(v, xmin), xmax)
+
+            fv = fn(v)
             
-            phi = a*np.random.uniform(-1,1,d)
-            
-            newbee = Bee()
-            newbee.loc = bee[j].loc + np.multiply(phi, (bee[j].loc - bee[k].loc))
-            newbee.loc = np.minimum(np.maximum(newbee.loc, xmin), xmax)
-            newbee.cost = fn(newbee.loc)
-            
-            if newbee.cost < bee[i].cost:
-                bee[i] = newbee
-            else:
-                C[i] += 1
+            if fv < onlooker[n].f:
+                onlooker[n] = Bee(x=v, f=fv)
+            # else:
+            #     C[n] += 1
         
         # Scout bees; send to the search area for discovering new food sources
-        # -> determine the scout bees -> send to possible food sources
-        for i in range(n):
-            if C[i] >= lim:
-                bee[i].loc = np.random.uniform(low=xmin, high=xmax, size=d)
-                bee[i].cost = fn(bee[i].loc)
-                C[i] = 0
-        
-        for i in range(n):
-            if bee[i].cost < best.cost:
-                best = bee[i]
+        # -> determine a scout bee -> send to possible food sources
+        # for i in range(SN):
+        #     if C[i] >= limit:
+        #         employed[i].x = np.random.uniform(low=xmin, high=xmax, size=d)
+        #         employed[i].f = fn(employed[i].x)
+        #         C[i] = 0
+        #         break
+        mask = C >= limit
+        tot_exh = sum(mask)
+        if tot_exh > 0:
+            i = np.random.choice(range(SN), p=mask/tot_exh)
+            employed[i].x = np.random.uniform(low=xmin, high=xmax, size=d)
+            employed[i].f = fn(employed[i].x)
+            C[i] = 0
+
+    best = Bee(f=float('inf'))
+    for i in range(SN):
+        if employed[i].f < best.f: best = employed[i]
+        if onlooker[i].f < best.f: best = onlooker[i]
     return best
 
 def greiwank(x):
@@ -132,8 +143,9 @@ if __name__=="__main__":
 
     d = 2
     for name, problem in zip(names, problems):
-        bound = (-4.5, 4.5, 1)
-        param = (50, 45)
+        bound = (-4.5, 4.5)
+        SN, MCN = 50, 45
+        limit = SN*d
 
-        best = ABC(problem, d, bound, param)
+        best = ABC(problem, d, bound, SN, limit, MCN)
         plot(problem, bound, best, name, n=100)
